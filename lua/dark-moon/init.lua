7110009e376744e4a5d6cd94e95d86bf1379f479
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-01-25 21:08:56 +0800

    Use `vim.g` directly to load terminal highlight groups

--- lua/dark-moon/init.lua
+++ lua/dark-moon/init.lua
@@ -4,9 +4,12 @@ local highlight = require("dark-moon.utils").highlight
 M.settings = function ()
   vim.cmd.highlight("clear")
   vim.opt.background = "dark"
-  vim.cmd.syntax("reset")
+
+  if vim.fn.exists("syntax_on") then
+    vim.cmd.syntax("reset")
+  end
+
   vim.g.colors_name = "dark-moon"
-  vim.opt.syntax = "ON"
   vim.opt.termguicolors = true
 end
 
@@ -16,20 +19,17 @@ M.set_hl_group = function (hl)
   end
 end
 
-M.set_var = function (group)
-  for var, color in pairs(group) do
-    vim.api.nvim_set_var(var, color)
-  end
-end
-
 M.load = function ()
   M.settings()
   M.set_hl_group(require("dark-moon.groups.editor").get())
   M.set_hl_group(require("dark-moon.groups.syntax").get())
-  M.set_var(require("dark-moon.groups.terminal").get())
   M.set_hl_group(require("dark-moon.groups.plugins.treesitter").get())
   M.set_hl_group(require("dark-moon.groups.plugins.cmp").get())
   M.set_hl_group(require("dark-moon.groups.plugins.gitsigns").get())
+
+  for terminal, color in pairs(require("dark-moon.groups.terminal").get()) do
+    vim.g[terminal] = color
+  end
 end
 
 return M
