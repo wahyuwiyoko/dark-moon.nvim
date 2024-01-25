c69af96888f4f85f0dd5c71eba2aa583320fd44c
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-01-25 17:17:05 +0800

    Breaking changes Treesitter highlight groups
    References:
    - https://github.com/nvim-treesitter/nvim-treesitter/issues/2293#issuecomment-1900679583
    - https://github.com/nvim-treesitter/nvim-treesitter/blob/master/CONTRIBUTING.md#highlights
    - https://github.com/nvim-treesitter/nvim-treesitter/commit/1ae9b0e4558fe7868f8cda2db65239cfb14836d0

--- lua/dark-moon/groups/plugins/treesitter.lua
+++ lua/dark-moon/groups/plugins/treesitter.lua
@@ -1,55 +1,126 @@
 local M = {}
 local C = require("dark-moon.colors")
+local U = require("dark-moon.utils")
 
 M.get = function ()
   return {
     -- See :help treesitter-highlight-groups
     -- Reference: https://github.com/nvim-treesitter/nvim-treesitter/blob/master/CONTRIBUTING.md
-    ["@text.literal"] = {},
-    ["@text.reference"] = {},
-    ["@text.title"] = {},
-    ["@text.uri"] = {},
-    ["@text.underline"] = {},
-    ["@text.todo"] = {},
-    ["@comment"] = { fg = C.fg_dark },
-    ["@punctuation"] = {},
+
+    -- Identifiers
+    ["@variable"] = {},
+    ["@variable.builtin"] = { fg = C.orange },
+    ["@variable.parameter"] = { fg = C.white },
+    ["@variable.member"] = { fg = C.blue_light },
+
     ["@constant"] = { fg = C.yellow },
     ["@constant.builtin"] = { fg = C.orange },
     ["@constant.macro"] = { fg = C.yellow },
-    ["@define"] = {},
-    ["@macro"] = {},
+
+    ["@module"] = { fg = C.yellow },
+    ["@module.builtin"] = {},
+    ["@label"] = { fg = C.cyan_dark },
+
+    -- Literals
     ["@string"] = { fg = C.green },
+    ["@string.documentation"] = {},
+    ["@string.regexp"] = { fg = C.cyan_dark },
     ["@string.escape"] = { fg = C.cyan_dark },
-    ["@string.special"] = {},
+    ["string.special"] = {},
+    ["string.special.symbol"] = { fg = C.orange_light },
+    ["string.special.url"] = { fg = C.green },
+    ["string.special.path"] = {},
+
     ["@character"] = {},
     ["@character.special"] = {},
-    ["@number"] = { fg = C.orange },
+
     ["@boolean"] = { fg = C.orange },
-    ["@float"] = {},
+    ["@number"] = { fg = C.orange },
+    ["@number.float"] = { fg = C.orange },
+
+    -- Types
+    ["@type"] = { fg = C.yellow },
+    ["@type.builtin"] = { fg = C.orange },
+    ["@type.definition"] = { fg = C.blue },
+    ["@type.qualifier"] = { fg = C.yellow },
+
+    ["@attribute"] = {},
+    ["@property"] = { fg = C.blue_light },
+
+    -- Functions
     ["@function"] = { fg = C.blue_light },
     ["@function.builtin"] = { fg = C.purple },
+    ["@function.call"] = { fg = C.blue_light },
     ["@function.macro"] = { fg = C.orange },
-    ["@parameter"] = {},
-    ["@method"] = {},
-    ["@field"] = {},
-    ["@property"] = { fg = C.blue_light },
+
+    ["@function.method"] = { fg = C.blue_light },
+    ["@function.method.call"] = { fg = C.blue_light },
+
     ["@constructor"] = { fg = C.yellow },
-    ["@conditional"] = { fg = C.blue },
-    ["@repeat"] = { fg = C.blue },
-    ["@label"] = { fg = C.cyan_dark },
     ["@operator"] = { fg = C.cyan },
+
+    -- Keywords
     ["@keyword"] = { fg = C.blue },
-    ["@exception"] = { fg = C.blue },
-    ["@variable"] = {},
-    ["@type"] = { fg = C.yellow },
-    ["@type.definition"] = { fg = C.blue },
-    ["@storageclass"] = {},
-    ["@structure"] = {},
-    ["@namespace"] = {},
-    ["@include"] = {},
-    ["@preproc"] = {},
-    ["@debug"] = { fg = C.cyan_dark },
-    ["@tag"] = { fg = C.blue_light }
+    ["@keyword.coroutine"] = {},
+    ["@keyword.function"] = { fg = C.blue_light },
+    ["@keyword.operator"] = { fg = C.cyan },
+    ["@keyword.import"] = { fg = C.cyan_dark },
+    ["@keyword.storage"] = {},
+    ["@keyword.repeat"] = { fg = C.blue },
+    ["@keyword.return"] = { fg = C.blue },
+    ["@keyword.debug"] = { fg = C.cyan_dark },
+    ["@keyword.exception"] = { fg = C.blue },
+
+    ["@keyword.conditional"] = { fg = C.blue },
+    ["@keyword.conditional.ternary"] = {},
+
+    ["@keyword.directive"] = { fg = C.yellow },
+    ["@keyword.directive.define"] = { fg = C.purple },
+
+    -- Punctuation
+    ["@punctuation.delimiter"] = { fg = C.white },
+    ["@punctuation.bracket"] = { fg = C.cyan_dark },
+    ["@punctuation.special"] = { fg = C.cyan },
+
+    -- Comments
+    ["@comment"] = { fg = C.fg_dark },
+    ["@comment.documentation"] = { fg = C.fg_dark },
+
+    ["@comment.error"] = { fg = C.red },
+    ["@comment.warning"] = { fg = C.purple },
+    ["@comment.todo"] = { fg = C.orange },
+    ["@comment.note"] = { fg = C.blue },
+
+    -- Markup
+    ["@markup.strong"] = {},
+    ["@markup.italic"] = {},
+    ["@markup.strikethrough"] = {},
+    ["@markup.underline"] = {},
+
+    ["@markup.heading"] = { bold = true },
+
+    ["@markup.quote"] = {},
+    ["@markup.math"] = {},
+    ["@markup.environment"] = {},
+
+    ["@markup.link"] = { fg = C.cyan },
+    ["@markup.link.label"] = { fg = C.cyan },
+    ["@markup.link.url"] = { fg = C.green },
+
+    ["@markup.raw"] = {},
+    ["@markup.raw.block"] = {},
+
+    ["@markup.list"] = { fg = C.cyan },
+    ["@markup.list.checked"] = {},
+    ["@markup.list.unchecked"] = {},
+
+    ["@diff.plus"] = { bg = U.darken(C.heavy_green, 0.3) },
+    ["@diff.minus"] = { bg = U.darken(C.heavy_red, 0.3) },
+    ["@diff.delta"] = { bg = U.darken(C.purple, 0.3) },
+
+    ["@tag"] = { fg = C.blue_light },
+    ["@tag.attribute"] = { fg = C.blue_light },
+    ["@tag.delimiter"] = { fg = C.cyan }
   }
 end
 
