90b8ac90903704ea7f83dcdc9f1415d5d3e505fe
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-01-27 03:31:07 +0800

    Update cmp & telescope highlight groups

--- lua/dark-moon/groups/integrations/cmp.lua
+++ lua/dark-moon/groups/integrations/cmp.lua
@@ -5,12 +5,12 @@ local M = {}
 M.get = function ()
   return {
     -- See :help cmp-highlight
-    CmpItemAbbr = { fg = C.white },
-    CmpItemAbbrDeprecated = { fg = C.red.base },
-    CmpItemAbbrMatch = { fg = C.white },
+    CmpItemAbbr = { fg = C.fg.base },
+    CmpItemAbbrMatch = { fg = C.fg.light },
     CmpItemAbbrMatchFuzzy = { fg = C.cyan.base },
-    CmpItemKind = { fg = C.cyan.base },
-    CmpItemMenu = { fg = C.cyan.base }
+    CmpItemAbbrDeprecated = { fg = C.fg.base, strikethrough = true },
+    CmpItemKind = { fg = C.cyan.dark },
+    CmpItemMenu = { fg = C.red.base }
   }
 end
 
--- lua/dark-moon/groups/integrations/telescope.lua
+++ lua/dark-moon/groups/integrations/telescope.lua
@@ -1,14 +1,16 @@
 local C = require("dark-moon.colors")
+local U = require("dark-moon.utils.colors")
 
 local M = {}
 
 M.get = function ()
   return {
     TelescopeTitle = { fg = C.fg.light, bold = true },
-    TelescopeBorder = { fg = C.fg.base, bg = C.bg.base },
+    TelescopeBorder = { fg = C.fg.base, bg = U.darken(C.bg.light, 0.3) },
     TelescopeNormal = { link = "NormalFloat" },
     TelescopeMatching = { fg = C.orange.base },
     TelescopePromptPrefix = { fg = C.fg.base },
+    TelescopeSelection = { fg = C.fg.light, bg = C.grey.dark },
     TelescopeSelectionCaret = { fg = C.fg.base }
   }
 end
